b928edfd0ae7a5e3d9e8ca843842b8fb65a6baa3
Gabe Giro <3791927+GabeGiro@users.noreply.github.com>
2025-07-23 09:50:42 +0200

    Added a useful comment

--- src/util/crop.py
+++ src/util/crop.py
@@ -1,3 +1,7 @@
+# Using round to avoid rounding errors
+# e.g., if height is 1000 and percentage is 0.8, start_row should be 200
+# With int, it could be 199 or 200 depending on the rounding
+
 def extract_right_part_of_image(image, percentage=0.7):
     height, width = image.shape[:2]
     start_column = round(width * (1 - percentage))
