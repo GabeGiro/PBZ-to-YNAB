6d1d464d989f53ea38c32415a1b17f6eff71d014
Gabe Giro <3791927+GabeGiro@users.noreply.github.com>
2025-07-23 09:41:37 +0200

    Removed unnecessary mocks in the crop tests

--- test/test_crop.py
+++ test/test_crop.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 import numpy as np
 import src.util.crop as crop_util
 
@@ -7,39 +6,24 @@ class TestUtilFunctions(unittest.TestCase):
     original_image_shape = (1000, 1000, 3)
     original_image = np.zeros(original_image_shape, dtype=np.uint8)
 
-    @patch("cv2.imread")
-    def test_extract_bottom_80_percent_of_the_image(self, mock_imread):
-        mock_imread.return_value = self.original_image
-        image = mock_imread.return_value
-        cropped = crop_util.extract_bottom_part_of_image(image, percentage=0.8)
+    def test_extract_bottom_80_percent_of_the_image(self):
+        cropped = crop_util.extract_bottom_part_of_image(self.original_image, percentage=0.8)
         self.assertEqual(cropped.shape[0], 800)
 
-    @patch("cv2.imread")
-    def test_extract_right_70_percent_part_of_the_image(self, mock_imread):
-        mock_imread.return_value = self.original_image
-        image = mock_imread.return_value
-        cropped = crop_util.extract_right_part_of_image(image, percentage=0.7)
-        self.assertEqual(cropped.shape[1], 700) 
+    def test_extract_right_70_percent_part_of_the_image(self):
+        cropped = crop_util.extract_right_part_of_image(self.original_image, percentage=0.7)
+        self.assertEqual(cropped.shape[1], 700)
 
-    @patch("cv2.imread")
-    def test_extract_right_80_percent_part_of_the_image(self, mock_imread):
-        mock_imread.return_value = self.original_image
-        image = mock_imread.return_value
-        cropped = crop_util.extract_right_part_of_image(image, percentage=0.8)
+    def test_extract_right_80_percent_part_of_the_image(self):
+        cropped = crop_util.extract_right_part_of_image(self.original_image, percentage=0.8)
         self.assertEqual(cropped.shape[1], 800)
 
-    @patch("cv2.imread")
-    def test_extract_left_30_percent_of_the_image(self, mock_imread):
-        mock_imread.return_value = self.original_image
-        image = mock_imread.return_value
-        cropped = crop_util.extract_left_part_of_image(image, percentage=0.3)
-        self.assertEqual(cropped.shape[1], 300) 
+    def test_extract_left_30_percent_of_the_image(self):
+        cropped = crop_util.extract_left_part_of_image(self.original_image, percentage=0.3)
+        self.assertEqual(cropped.shape[1], 300)
 
-    @patch("cv2.imread")
-    def test_extract_left_80_percent_of_the_image(self, mock_imread):
-        mock_imread.return_value = self.original_image
-        image = mock_imread.return_value
-        cropped = crop_util.extract_left_part_of_image(image, percentage=0.8)
+    def test_extract_left_80_percent_of_the_image(self):
+        cropped = crop_util.extract_left_part_of_image(self.original_image, percentage=0.8)
         self.assertEqual(cropped.shape[1], 800)
 
 if __name__ == '__main__':
